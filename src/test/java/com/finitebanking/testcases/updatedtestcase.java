4ef8dd1ff7a2a02075776c5d129970fa92620538
Raviteja <raviteja209209@gmail.com>
2021-06-19 15:41:08 +0530

    added new test case

---
+++ src/test/java/com/finitebanking/testcases/updatedtestcase.java
@@ -0,0 +1,15 @@
+package com.finitebanking.testcases;
+
+import com.finitebanking.pageobjects.LoginPage;
+
+public class updatedtestcase {
+
+}
+LoginPage lp=new LoginPage(driver);
+lp.setusername(user);
+logger.info("user name provided");
+lp.setpassword(pwd);
+logger.info("password provided");
+lp.clickSubmit();
+
+Thread.sleep(3000);
